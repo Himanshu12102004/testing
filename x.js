311650a7baa4e1a2a4177bb2a419f30b9cb59f41
Himanshu Gupta <himanshu12102004@gmail.com>
2024-03-21 07:25:07 +0530

    some changes for geolocation

--- x.js
+++ x.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
   res.json(responseData);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
